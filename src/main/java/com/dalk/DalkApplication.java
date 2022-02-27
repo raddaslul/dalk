e534e4a65c0ceb2e70fb47ce8b03b1e29ab04f31
raddaslul <raddaslul@naver.com>
2022-02-27 21:52:11 +0900

    S3 구현

--- src/main/java/com/dalk/DalkApplication.java
+++ src/main/java/com/dalk/DalkApplication.java
@@ -1,15 +1,21 @@
 package com.dalk;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class DalkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DalkApplication.class, args);
-    }
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
 
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(DalkApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 }
