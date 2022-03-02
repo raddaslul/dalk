ed8d13b350d6090f72174046d18a3b972cec1d22
raddaslul <raddaslul@naver.com>
2022-03-03 02:23:32 +0900

    web socket refactoring

--- src/main/java/com/dalk/service/ChatMessageService.java
+++ src/main/java/com/dalk/service/ChatMessageService.java
@@ -1,7 +1,13 @@
 package com.dalk.service;
 
 import com.dalk.domain.ChatMessage;
+import com.dalk.domain.User;
+import com.dalk.dto.requestDto.ChatMessageRequestDto;
+import com.dalk.dto.responseDto.ChatMessageResponseDto;
+import com.dalk.dto.responseDto.ItemResponseDto;
+import com.dalk.dto.responseDto.UserInfoResponseDto;
 import com.dalk.repository.ChatMessageRepository;
+import com.dalk.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,14 +16,19 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 @RequiredArgsConstructor
+@Service
 public class ChatMessageService {
 
     private final ChannelTopic channelTopic;
     private final RedisTemplate redisTemplate;
+    private final UserRepository userRepository;
     private final ChatMessageRepository chatMessageRepository;
-    private final UserService userService;
 
     // destination 정보에서 roomId 추출
     public Long getRoomId(String destination) {
@@ -31,28 +42,40 @@ public class ChatMessageService {
         }
     }
 
-    // 채팅방에 메시지 발송
-    public void sendChatMessage(ChatMessage chatMessage) {
-        if (ChatMessage.MessageType.ENTER.equals(chatMessage.getType())) {
-            chatMessage.setMessage(chatMessage.getSender() + "님이 방에 입장했습니다.");
-            chatMessage.setSender("[알림]");
-        } else if (ChatMessage.MessageType.QUIT.equals(chatMessage.getType())) {
-            chatMessage.setMessage(chatMessage.getSender() + "님이 방에서 나갔습니다.");
-            chatMessage.setSender("[알림]");
+    public ChatMessage save(ChatMessageRequestDto chatMessageRequestDto) {
+        // 메시지 생성 시간 삽입
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm");
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        String dateResult = sdf.format(date);
+        chatMessageRequestDto.setCreatedAt(dateResult);
+
+        // 유저 정보 삽입
+        User user = userRepository.findById(chatMessageRequestDto.getUserId())
+                .orElseThrow(IllegalAccessError::new);
+
+        if (ChatMessage.MessageType.ENTER.equals(chatMessageRequestDto.getType())) {
+            chatMessageRequestDto.setMessage(user.getNickname() + "님이 방에 입장했습니다.");
+//            chatMessage.setSender("[알림]");
+        } else if (ChatMessage.MessageType.QUIT.equals(chatMessageRequestDto.getType())) {
+            chatMessageRequestDto.setMessage(user.getNickname() + "님이 방에서 나갔습니다.");
+//            chatMessage.setSender("[알림]");
         }
-        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
+
+        ChatMessage chatMessage = new ChatMessage(chatMessageRequestDto, user);
+        return chatMessageRepository.save(chatMessage);
     }
 
-    public void save(ChatMessage chatMessage) {
-        ChatMessage message = new ChatMessage();
-        message.setType(chatMessage.getType());
-        message.setRoomId(chatMessage.getRoomId());
-        message.setUser(userService.findById(chatMessage.getUserId()));
-        message.setUserId(chatMessage.getUserId());
-        message.setSender(chatMessage.getSender());
-        message.setMessage(chatMessage.getMessage());
-        message.setCreatedAt(chatMessage.getCreatedAt());
-        chatMessageRepository.save(message);
+    // 채팅방에 메시지 발송
+    public void sendChatMessage(ChatMessage chatMessage) {
+        User user = userRepository.findById(chatMessage.getUser().getId())
+                .orElseThrow(IllegalAccessError::new);
+
+        ItemResponseDto itemResponseDto = new ItemResponseDto(user);
+        UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto(user, itemResponseDto);
+        ChatMessageResponseDto chatMessageResponseDto = new ChatMessageResponseDto(chatMessage, userInfoResponseDto);
+        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessageResponseDto);
     }
 
     public Page<ChatMessage> getChatMessageByRoomId(String roomId, Pageable pageable) {
