5d7f6e0d20a0b050335c3581346f3f040b096de7
raddaslul <raddaslul@naver.com>
2022-03-03 14:21:03 +0900

    item 테이블에 timestamped import

--- src/main/java/com/dalk/domain/Item.java
+++ src/main/java/com/dalk/domain/Item.java
@@ -1,6 +1,6 @@
 package com.dalk.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.dalk.domain.time.Timestamped;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
