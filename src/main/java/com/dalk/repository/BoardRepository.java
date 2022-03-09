30c4498c063b839123550c0711e8d0e94dec97e1
신동석 <96603945+dss1222@users.noreply.github.com>
2022-03-09 18:04:49 +0900

    검색기능 수정

--- src/main/java/com/dalk/repository/BoardRepository.java
+++ src/main/java/com/dalk/repository/BoardRepository.java
@@ -11,5 +11,5 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 //    @Query("select p from Board p where p.topicA like %:keyword% or p.topicB like %:keyword%")
 //    List<Board> findSearch(String keyword);
     List<Board> findAllByTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String keyword1, String keyword2);
-
+    List<Board> findDistinctByCategorys_CategoryOrTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String category1 ,String category2, String category3);
 }
