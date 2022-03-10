6cb9ad9c8ac850854f80f548109f75b28878aa1a
raddaslul <raddaslul@naver.com>
2022-03-10 12:05:48 +0900

    게시글 최신 생성순으로 조회

--- src/main/java/com/dalk/repository/BoardRepository.java
+++ src/main/java/com/dalk/repository/BoardRepository.java
@@ -12,4 +12,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 //    List<Board> findSearch(String keyword);
     List<Board> findAllByTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String keyword1, String keyword2);
     List<Board> findDistinctByCategorys_CategoryOrTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String category1 ,String category2, String category3);
+
+    List<Board> findAllByOrderByCreatedAtDesc();
 }
