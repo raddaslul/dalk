21661f9c4d0508bb5c23e8dc981bae4aff9ef9bb
신동석 <96603945+dss1222@users.noreply.github.com>
2022-03-10 23:53:55 +0900

    아이템 종류 추가. 리버스 완성. 민기서비스 아이템서비스랑 합체

--- src/main/java/com/dalk/dto/responseDto/ItemResponseDto.java
+++ src/main/java/com/dalk/dto/responseDto/ItemResponseDto.java
@@ -11,10 +11,14 @@ public class ItemResponseDto {
     private Integer bigFont;
     private Integer onlyMe;
     private Integer myName;
+    private Integer papago;
+    private Integer reverse;
 
     public ItemResponseDto(User user) {
         this.bigFont=user.getItem().getBigFont();
         this.onlyMe=user.getItem().getOnlyMe();
         this.myName=user.getItem().getMyName();
+        this.myName=user.getItem().getPapago();
+        this.myName=user.getItem().getReverse();
     }
 }
