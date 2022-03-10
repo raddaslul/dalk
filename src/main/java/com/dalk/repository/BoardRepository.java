d55a69983571ed278a2e1620f7a75256ead55642
신동석 <96603945+dss1222@users.noreply.github.com>
2022-03-10 13:40:35 +0900

    뽑기기능 수정 연관관계 정리

--- src/main/java/com/dalk/repository/BoardRepository.java
+++ src/main/java/com/dalk/repository/BoardRepository.java
@@ -7,9 +7,6 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-//    @Modifying
-//    @Query("select p from Board p where p.topicA like %:keyword% or p.topicB like %:keyword%")
-//    List<Board> findSearch(String keyword);
-    List<Board> findAllByTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String keyword1, String keyword2);
+
     List<Board> findDistinctByCategorys_CategoryOrTopicAContainingIgnoreCaseOrTopicBContainingIgnoreCase(String category1 ,String category2, String category3);
 }
