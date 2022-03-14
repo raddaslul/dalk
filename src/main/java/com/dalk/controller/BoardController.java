0909b67b9a4e502930bfe88aad999f2e79d8e893
hyeonjh <jhoon3697@naver.com>
2022-03-14 13:46:16 +0900

    페이징처리

--- src/main/java/com/dalk/controller/BoardController.java
+++ src/main/java/com/dalk/controller/BoardController.java
@@ -7,11 +7,15 @@ import com.dalk.security.UserDetailsImpl;
 import com.dalk.service.BoardService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -22,8 +26,14 @@ public class BoardController {
 
     @GetMapping("/api/boards")
     @ApiOperation(value = "게시글 전체 조회")
-    public List<MainPageBoardResponseDto> getMainPageBoard() {
-        return boardService.getMainPageBoard();
+    public List<MainPageBoardResponseDto> getMainPageBoard(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+//            @RequestParam("sortBy") String sortBy,
+//            @RequestParam("isAsc") boolean isAsc
+        ){
+
+        return boardService.getMainPageBoard(page,size);
     }
 
     @GetMapping("/api/boards/{boardId}")
--- src/main/java/com/dalk/service/BoardService.java
+++ src/main/java/com/dalk/service/BoardService.java
@@ -12,6 +12,10 @@ import com.dalk.repository.*;
 import com.dalk.repository.wl.WarnBoardRepository;
 import com.dalk.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -60,8 +64,14 @@ public class BoardService {
     }
 
     //게시글 전체 조회
-    public List<MainPageBoardResponseDto> getMainPageBoard() {
-        List<Board> boardList = boardRepository.findAllByOrderByCreatedAtDesc();
+
+    public List<MainPageBoardResponseDto> getMainPageBoard(int page, int size) {
+//        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC ;
+//        Sort sort = Sort.by(direction,sortBy);
+        Pageable pageable = PageRequest.of(page,size);
+//        Pageable pageable1 =new PageRequest(page,size,sort);
+
+        Page<Board> boardList = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
         List<MainPageBoardResponseDto> mainPageBoardResponseDtoList = new ArrayList<>();
 
         for (Board board : boardList) {
@@ -75,6 +85,24 @@ public class BoardService {
         }
         return mainPageBoardResponseDtoList;
     }
+
+
+
+//    public List<MainPageBoardResponseDto> getMainPageBoard(int page,int size) {
+//        List<Board> boardList = boardRepository.findAllByOrderByCreatedAtDesc();
+//        List<MainPageBoardResponseDto> mainPageBoardResponseDtoList = new ArrayList<>();
+//
+//        for (Board board : boardList) {
+//            List<Category> categoryList = categoryRepository.findCategoryByBoard(board);
+//            User user = userRepository.findById(board.getCreateUserId()).orElseThrow(
+//                    () -> new LoginUserNotFoundException("유저 정보가 없습니다")
+//            );
+//            List<WarnBoard> warnBoardList = warnBoardRepository.findByBoardId(board.getId());
+//            MainPageBoardResponseDto mainPageBoardResponseDto = new MainPageBoardResponseDto(board, ItemService.categoryStringList(categoryList), user, warnBoardList.size(), null);
+//            mainPageBoardResponseDtoList.add(mainPageBoardResponseDto);
+//        }
+//        return mainPageBoardResponseDtoList;
+//    }
 
     //게시글 상세 조회
     public DetailResponseDto getMainPageBoardDetail(Long boardId) {
