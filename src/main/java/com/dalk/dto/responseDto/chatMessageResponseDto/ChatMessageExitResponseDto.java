2c4814a59a82eac0ddecacbea072fbca0aca4347
raddaslul <raddaslul@naver.com>
2022-03-15 21:13:56 +0900

    유저가 채팅방에서 나갈 시 유저정보도 response에 포함

--- src/main/java/com/dalk/dto/responseDto/chatMessageResponseDto/ChatMessageExitResponseDto.java
+++ src/main/java/com/dalk/dto/responseDto/chatMessageResponseDto/ChatMessageExitResponseDto.java
@@ -1,7 +1,9 @@
 package com.dalk.dto.responseDto.chatMessageResponseDto;
 
 import com.dalk.domain.ChatMessage;
+import com.dalk.domain.User;
 import com.dalk.dto.requestDto.ChatMessageRequestDto;
+import com.dalk.dto.responseDto.UserInfoResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,12 @@ public class ChatMessageExitResponseDto {
     private ChatMessage.MessageType type;
     private String roomId;
     private String message;
+    private UserInfoResponseDto userInfo;
 
-    public ChatMessageExitResponseDto(ChatMessageRequestDto chatMessageRequestDto) {
+    public ChatMessageExitResponseDto(ChatMessageRequestDto chatMessageRequestDto, User user) {
         this.type = chatMessageRequestDto.getType();
         this.roomId = chatMessageRequestDto.getRoomId();
         this.message = chatMessageRequestDto.getMessage();
+        this.userInfo = new UserInfoResponseDto(user);
     }
 }
