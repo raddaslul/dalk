93902db50dbe1687126db025f4b02857685e1d4c
hyeonjh <jhoon3697@naver.com>
2022-03-26 15:42:04 +0900

    ex수정

--- src/main/java/com/dalk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dalk/exception/GlobalExceptionHandler.java
@@ -19,38 +19,28 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("U001", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(PasswordContainsUsernameException.class)
-    public ResponseEntity<ErrorResponse> handlePasswordContainsUsernameException(PasswordContainsUsernameException e) {
-        return new ResponseEntity<>(new ErrorResponse("U002", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(DuplicateUsernameException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateUsernameException(DuplicateUsernameException e) {
-        return new ResponseEntity<>(new ErrorResponse("U003", e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("U002", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DuplicationNicknameException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateNicknameException(DuplicationNicknameException e) {
-        return new ResponseEntity<>(new ErrorResponse("U004", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(PasswordNotEqualException.class)
-    public ResponseEntity<ErrorResponse> handlePasswordNotEqualException(PasswordNotEqualException e) {
-        return new ResponseEntity<>(new ErrorResponse("U005", e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("U003", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(LoginUserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleLoginUserNotFoundException(LoginUserNotFoundException e) {
-        return new ResponseEntity<>(new ErrorResponse("U006", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("U004", e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
-        return new ResponseEntity<>(new ErrorResponse("U007", e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse("U005", e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
     @ExceptionHandler(WarnDuplicateException.class)
     public ResponseEntity<ErrorResponse> handleWarnDuplicateException(WarnDuplicateException e) {
-        return new ResponseEntity<>(new ErrorResponse("U008", e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("U006", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalTokenUsernameException.class)
@@ -58,19 +48,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("T001", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(IllegalTokenUsernameDateException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalTokenDateException(IllegalTokenUsernameDateException e) {
+    @ExceptionHandler(IllegalTokenUserIdException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalTokenUserIdException(IllegalTokenUserIdException e) {
         return new ResponseEntity<>(new ErrorResponse("T002", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(IllegalTokenUserIdException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalTokenUserIdException(IllegalTokenUserIdException e) {
+    @ExceptionHandler(IllegalTokenDateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalTokenDateException(IllegalTokenDateException e) {
         return new ResponseEntity<>(new ErrorResponse("T003", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalTokenUserIdDateException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalTokenUserIdDateException(IllegalTokenUserIdDateException e) {
-        return new ResponseEntity<>(new ErrorResponse("T004", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BoardNotFoundException.class)
@@ -78,18 +63,9 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("B001", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(IllegalBoardUpdateUserException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalBoardUpdateUserException(IllegalBoardUpdateUserException e) {
-        return new ResponseEntity<>(new ErrorResponse("B002", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalPostDeleteUserException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalPostDeleteUserException(IllegalPostDeleteUserException e) {
-        return new ResponseEntity<>(new ErrorResponse("B003", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
     @ExceptionHandler(WarnBoardDuplicateException.class)
     public ResponseEntity<ErrorResponse> handleWarnBoardDuplicateException(WarnBoardDuplicateException e) {
-        return new ResponseEntity<>(new ErrorResponse("B004", e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("B002", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
@@ -97,18 +73,9 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("C001", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(IllegalCommentUpdateUserException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalCommentUpdateUserException(IllegalCommentUpdateUserException e) {
-        return new ResponseEntity<>(new ErrorResponse("C002", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalCommentDeleteUserException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalCommentDeleteUserException(IllegalCommentDeleteUserException e) {
-        return new ResponseEntity<>(new ErrorResponse("C003", e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
     @ExceptionHandler(WarnCommentDuplicateException.class)
     public ResponseEntity<ErrorResponse> handleWarnCommentDuplicateException(WarnCommentDuplicateException e) {
-        return new ResponseEntity<>(new ErrorResponse("C004", e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("C002", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ItemNotFoundException.class)
---
+++ src/main/java/com/dalk/exception/ex/IllegalTokenDateException.java
@@ -0,0 +1,7 @@
+package com.dalk.exception.ex;
+
+public class IllegalTokenDateException extends RuntimeException{
+    public IllegalTokenDateException(String message) {
+        super(message);
+    }
+}
